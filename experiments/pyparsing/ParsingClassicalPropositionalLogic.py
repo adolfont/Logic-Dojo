51618a902229d3748ad78669ce020c0312e4b13a
Adolfo Neto (netbook fedora 13) <adolfo.usp@gmail.com>
2011-03-14 11:21:36 -0300

    Corrected version of the parser for CPL

--- experiments/pyparsing/ParsingClassicalPropositionalLogic.py
+++ experiments/pyparsing/ParsingClassicalPropositionalLogic.py
@@ -9,8 +9,10 @@
 # In the book the author uses Python 3 and pyparsing_py3
 
 
-from pyparsing import (alphanums, alphas, delimitedList, Forward, Group, Keyword, Literal, opAssoc, 
-	operatorPrecedence, ParserElement, ParseException, ParseSyntaxException, Suppress, Word)
+from pyparsing import (alphanums, alphas, delimitedList, Forward, Group,
+	 Keyword, Literal, opAssoc, operatorPrecedence, ParserElement,
+	 ParseException, ParseSyntaxException, Suppress, Word)
+
 ParserElement.enablePackrat()
 
 left_parenthesis, right_parenthesis = map(Suppress, "()")
@@ -18,7 +20,6 @@ implies = Literal("->")
 or_ = Literal("|")
 and_ = Literal("&")
 not_ = Literal("!") | Literal ("~")
-equals = Literal("=")
 boolean = Keyword("false") | Keyword("true")
 symbol = Word(alphas, alphanums)
 
@@ -26,18 +27,47 @@ formula = Forward()
 operand = boolean | symbol
 
 formula << operatorPrecedence(operand, [
-				(equals, 2, opAssoc.LEFT),
 				(not_, 1, opAssoc.RIGHT),
 				(and_, 2, opAssoc.LEFT),
 				(or_, 2, opAssoc.LEFT),
 				(implies, 2, opAssoc.RIGHT)])
 
+def parse(text):
 
-text="A&B&C->D"
-try:
-	result = formula.parseString(text, parseAll=True)
-	assert len(result) == 1
-	print result[0].asList()
-except (ParseException, ParseSyntaxException) as err:
-	print("Syntax error:\n{0.line}\n{1}^".format(err, " " * (err.column - 1)))
+	try:
+		result = formula.parseString(text, parseAll=True)
+		assert len(result) == 1
+		if isinstance(result[0],str):
+			return result[0]
+		else:
+			return result[0].asList()
+	except (ParseException, ParseSyntaxException) as err:
+		print("Syntax error:\n{0.line}\n{1}^".format(err, " " * (err.column - 1)))
+		return ""
+
+
+
+sample_formulas=[
+	"A",
+	"A1",
+	"!A1",
+	"!!A1",
+	"A&B",
+	"A|B",
+	"A->B",
+	"A|!B",
+	"!A|B",
+	"A->!!B",
+	"!A&!B",
+	"!A!B",
+]
+
+for sample_formula in sample_formulas:
+	print sample_formula,  " ===> ", parse(sample_formula)
+
+print "This is the end..."
+
+
+
+
 
