ed4c9c17061768bb2b4f4c228145d52c2b328708
Adolfo Neto (netbook fedora 13) <adolfo.usp@gmail.com>
2011-03-24 13:05:09 -0300

    Some more tests and features for the Formula and FormulaFactory classes

--- experiments/pyparsing/RigorousClassicalPropositionalLogicParser/Formula.py
+++ experiments/pyparsing/RigorousClassicalPropositionalLogicParser/Formula.py
@@ -34,6 +34,24 @@ class AtomicFormula(Formula):
 	def __str__(self):
 		return self.atom
 
+class UnaryFormula(Formula):
+	def __init__(self, connective, subformula):
+		self.connective = connective
+		self.subformula = subformula
+	def getConnective(self):
+		return self.connective
+	def __str__(self):
+		return str(self.connective) + str(self.subformula)
+
+class BinaryFormula(Formula):
+	def __init__(self, connective, leftSubformula, rightSubformula):
+		self.connective = connective
+		self.leftSubformula = leftSubformula
+		self.rightSubformula = rightSubformula
+	def getConnective(self):
+		return self.connective
+	def __str__(self):
+		return "(" + str(self.leftSubformula) + str(self.connective) + str(self.rightSubformula) + ")"
 
 class FormulaFactory:
 	def __init__(self):
@@ -45,4 +63,6 @@ class FormulaFactory:
 	def createUnaryFormula(self, connective, subformula):
 		return UnaryFormula(connective, subformula)
 
+	def createBinaryFormula(self, connective, leftSubformula, rightSubformula):
+		return BinaryFormula(connective, leftSubformula, rightSubformula)
 		
--- experiments/pyparsing/RigorousClassicalPropositionalLogicParser/FormulaTests.py
+++ experiments/pyparsing/RigorousClassicalPropositionalLogicParser/FormulaTests.py
@@ -7,11 +7,46 @@ from Formula import *
 import unittest
  
 class Formula_TestCase(unittest.TestCase):
+	def setUp(self):
+		self.formulaFactory = FormulaFactory()
+		self.anAtomicFormula = self.formulaFactory.createAtomicFormula("A")
+		self.anUnaryFormula = self.formulaFactory.createUnaryFormula("!",self.anAtomicFormula)
+		self.aComplexityThreeUnaryFormula = self.formulaFactory.createUnaryFormula("!",self.anUnaryFormula)
+		self.aBinaryFormula = self.formulaFactory.createBinaryFormula("&",self.anAtomicFormula, self.anAtomicFormula)
+		self.anotherAtomicFormula = self.formulaFactory.createAtomicFormula("B")
+		self.anotherBinaryFormula = self.formulaFactory.createBinaryFormula("&",self.anAtomicFormula, self.anotherAtomicFormula)
+		self.aComplexitySixFormula = self.formulaFactory.createBinaryFormula("->", self.anUnaryFormula, self.anotherBinaryFormula)
+
+
 	def testAtomicFormula_getConnective(self):
-		self.assertEquals(None,FormulaFactory().createAtomicFormula("A").getConnective())
+		self.assertEquals(None,self.anAtomicFormula.getConnective())
 
 	def testAtomicFormula_toString(self):
-		self.assertEquals("A",str(FormulaFactory().createAtomicFormula("A")))
+		self.assertEquals("A",str(self.anAtomicFormula))
+
+	def testComplexityTwoUnaryFormula_getConnective(self):
+		self.assertEquals("!",self.anUnaryFormula.getConnective())
+
+	def testComplexityTwoUnaryFormula_toString(self):
+		self.assertEquals("!A",str(self.anUnaryFormula))
+
+	def testComplexityThreeUnaryFormula_getConnective(self):
+		self.assertEquals("!",self.aComplexityThreeUnaryFormula.getConnective())
+
+	def testComplexityThreeUnaryFormula_toString(self):
+		self.assertEquals("!!A",str(self.aComplexityThreeUnaryFormula))
+
+	def testComplexityThreeBinaryFormula_getConnective(self):
+		self.assertEquals("&",self.aBinaryFormula.getConnective())
+		
+	def testComplexityThreeBinaryFormula_toString(self):
+		self.assertEquals("(A&A)",str(self.aBinaryFormula))
+
+	def testComplexitySixBinaryFormula_getConnective(self):
+		self.assertEquals("->",self.aComplexitySixFormula.getConnective())
+		
+	def testComplexitySixBinaryFormula_toString(self):
+		self.assertEquals("(!A->(A&B))",str(self.aComplexitySixFormula))
 
 
 if __name__ == '__main__':
